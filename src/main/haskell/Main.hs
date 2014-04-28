9c1ea7cef55df3032807bd7d9c4969b1652b9569
Michael Pearson <michael.oliver.pearson@gmail.com>
2014-04-28 20:01:08 +0100

    updates

--- src/main/haskell/Main.hs
+++ src/main/haskell/Main.hs
@@ -1,6 +1,7 @@
-main :: IO()
-main = putStrLn $ show euler6
+import Data.List
 
+main :: IO()
+main = putStrLn $ show euler5
 
 euler1 = sum $ filter (multipleOf3 `merge` multipleOf5) [1..999]
 
@@ -10,15 +11,21 @@ euler3 = head $ reverse $ primeFactors 600851475143
 
 euler4 = maximum $ filter palindrome threeDigitProducts
 
-euler5 = (squareSums 100) - (sumSquares 100)
+euler5 = product $ (simplify.reverse.sort.concat) $ arrangePrimeFactors [2..20]
 
-euler6 = primes !! 10001
+euler6 = (squareSums 100) - (sumSquares 100)
+
+euler7 = primes !! 10000
+
+
+arrangePrimeFactors :: [Integer] -> [[(Integer, Int)]]
+arrangePrimeFactors = map (countDuplicates.sort.primeFactors)
 
 multipleOf3 :: Integer -> Bool
-multipleOf3 x = x `mod` 3 == 0
+multipleOf3 x = 3 `divides` x
 
 multipleOf5 :: Integer -> Bool
-multipleOf5 x = x `mod` 5 == 0
+multipleOf5 x = 5 `divides` x
 
 merge :: (a -> Bool) -> (a -> Bool) -> a -> Bool
 merge f g x = (f x) || (g x) 
@@ -29,7 +36,7 @@ fibs = fibProducer 0 1
 fibProducer :: Integer -> Integer -> [Integer]
 fibProducer a b = a : (fibProducer b (a+b))
 
-primeFactors n = filter (\x -> n `mod` x == 0) $ takeWhile (\x -> x * x <= n) primes
+primeFactors n = if prime primes n then [n] else filter (swap divides n) $ takeWhile (\x -> x * x <= n) primes
 
 primes :: [Integer]
 primes = 2 : filter (prime primes) [3..]
@@ -38,20 +45,38 @@ prime :: [Integer] -> Integer -> Bool
 prime [] n = True
 prime (x:xs) n
   | x * x > n  = True 
-  | n `mod` x == 0 = False
+  | x `divides` n = False
   | otherwise = prime xs n
 
 palindrome :: Integer -> Bool
 palindrome x = (show x) == (reverse $ show x)
 
+countDuplicates :: [Integer] -> [(Integer, Int)]
+countDuplicates [] = []
+countDuplicates (x:xs) = let (this, others) = partition (x ==) (x:xs) in (x, length this) : countDuplicates others
+
 threeDigitProducts :: [Integer]
 threeDigitProducts = [a*b | a <- [100..999], b <- [100..999]]
 
+simplify :: [(Integer, Int)] -> [Integer]
+simplify [] = []
+simplify ((x, c) : xs) = (replicate c x) ++ simplify (filter ((x ==).fst) xs)
+
 square :: Integer -> Integer
 square x = x * x
+
+swap :: (a -> b -> c) -> b -> a -> c
+swap f x y = f y x
 
 sumSquares :: Integer -> Integer
 sumSquares n = sum $ map square [1..n]
 
 squareSums :: Integer -> Integer
 squareSums n = square $ sum [1..n]
+
+dividesByAll :: [Integer] -> Integer -> Bool
+dividesByAll [] n = True
+dividesByAll (x:xs) n = if x `divides` n then dividesByAll xs n else False
+
+divides :: Integer -> Integer -> Bool
+divides a b = b `mod` a == 0
